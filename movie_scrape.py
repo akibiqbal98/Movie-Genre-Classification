49f8cc11e96712d35a0ba8a1ad13289f9c26acc7
akibiqbal98 <akibiqbal98@gmail.com>
2023-02-08 20:12:41 +0600

    new

--- movie_scrape.py
+++ movie_scrape.py
@@ -37,6 +37,23 @@ driver.get('https://www.imdb.com/search/title/?title_type=feature&release_date=2
 data = []
 
 
+def create_driver_and_wait(waiting_time=10):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    
+    # Create an instance of Chrome
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # wait for the page to load
+    driver.implicitly_wait(waiting_time)
+    wait = WebDriverWait(driver, waiting_time)
+
+    # Create an instance of Chrome
+    return driver, wait
+
+
 def get_innerHTML(element: WebElement):
     # name = element.find_element(By.CSS_SELECTOR, "div#main h3 a").text
     year = element.find_element(By.CSS_SELECTOR, "div#main h3 span.lister-item-year").text
@@ -53,22 +70,22 @@ def get_innerHTML(element: WebElement):
         "Rating": element.find_element(By.CSS_SELECTOR, "div#main div.ratings-bar strong").text,
         "Description": description.text , 
         "Genre": genre.find_element(By.CSS_SELECTOR,"span.genre").text.split(",")
+        
     }
 
 
-# def change_page(by, value):#
-#     element: WebElement = wait.until(EC.presence_of_element_located((by, value)))
-#     print(element)
-#     driver.execute_script("arguments[0].click();", element)
-#     sleep(30)
+def change_page(by, value):
+    element: WebElement = wait.until(EC.presence_of_element_located((by, value)))
+    driver.execute_script("arguments[0].click();", element)
+    sleep(20)
 
-def change_page():
-    # next page button
-    XPATH = "//div[@id='main']/div[@class='desc']/a[@class='lister-page-next next-page']"
-    element: WebElement = wait.until(EC.presence_of_element_located((By.XPATH, XPATH)))
-    # print(element_.get_attribute("outerHTML"))
-    # element.click()  # Not working
-    driver.execute_script("arguments[0].click();", element) 
+# def change_page():
+#     # next page button
+#     XPATH = "//div[@id='main']/div[@class='desc']/a[@class='lister-page-next']"
+#     element: WebElement = wait.until(EC.presence_of_element_located((By.XPATH, XPATH)))
+#     # print(element_.get_attribute("outerHTML"))
+#     # element.click()  # Not working
+#     driver.execute_script("arguments[0].click();", element) 
 
 
 # Navigate to a website
@@ -82,14 +99,14 @@ driver.get(url)
 
 all_elements = []
 
-page_no = 3
+page_no = 174
 
 for indx in range(page_no):
     while(True):
         try:
             elements: WebElement = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div#main div.lister-item')))
             all_elements.extend(list(map(get_innerHTML, elements[:])))
-        
+            
         except Exception as e:
             print(e)
             continue
@@ -97,8 +114,8 @@ for indx in range(page_no):
         else:
             print(len(all_elements))
             if indx != page_no - 1:
-                change_page()
-                sleep(50) 
+                change_page(By.CSS_SELECTOR,  'div#main div.desc a.lister-page-next')
+                sleep(10) 
             break
 
 print(all_elements)
